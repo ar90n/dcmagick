17298ecbd4620170af7142e2c67d31b28fbc6f29
Masahiro Wada <argon.argon.argon@gmail.com>
2018-12-04 04:04:47 +0900

    Refactor dump.py

--- src/dcmagick/subcommands/dump.py
+++ src/dcmagick/subcommands/dump.py
@@ -19,56 +19,58 @@ class Format(Enum):
 def dump_pretty(dcm):
     return str(dcm)
 
-def repval(self):
-    def r(v):
+
+def dump_value(value):
+    def repr_value(value):
         import numbers
-        if isinstance(v, numbers.Real) or isinstance(v, bool):
-            return v
-        elif isinstance(v, str):
-            return v.rstrip('\x00\u0000')
-        else:
-            return repr(v).rstrip('\x00').replace('\u0000','')
+        if isinstance(value, numbers.Real) or isinstance(value, bool):
+            return value 
 
+        if not isinstance(value ,str):
+            value = repr(value)
+        return value.rstrip('\x00\u0000')
 
-    vvm = datadict.get_entry(self.tag)[1].split('-')
+    def is_array_value(value):
+        """
+        Return a str representation of the element's `value`.
+        The following codes from pydicom repository.
+        """
+        byte_VRs = ['OB', 'OW', 'OW/OB', 'OW or OB', 'OB or OW',
+                    'US or SS or OW', 'US or SS']
+        return (value.VR in byte_VRs and len(value.value) > value.maxBytesToDisplay)
 
-    """Return a str representation of the element's `value`."""
-    byte_VRs = ['OB', 'OW', 'OW/OB', 'OW or OB', 'OB or OW',
-                'US or SS or OW', 'US or SS']
-    if (self.VR in byte_VRs and len(self.value) > self.maxBytesToDisplay):
-        repVal = "Array of %d bytes" % len(self.value)
-    elif 1 < self.VM:
-        repVal = [r(v) for v in self]
-    elif len(vvm) == 2 or 1 < int(vvm[0]):
-        repVal = [r(self.value)]
-    elif isinstance(self.value, UID):
-        repVal = self.value.name
+    def is_multiple_values(value):
+        vvm = datadict.get_entry(value.tag)[1].split('-')
+        return len(vvm) == 2 or 1 < int(vvm[0])
+
+    if is_array_value(value):
+        repVal = "Array of %d bytes" % len(value.value)
+    elif is_multiple_values(value):
+        repVal = [repr_value(v) for v in value] if 1 < value.VM else [repr_value(value.value)]
+    elif isinstance(value.value, UID):
+        repVal = value.value.name
     else:
-        repVal = r(self.value)  # will tolerate unicode too
+        repVal = repr_value(value.value)
     return repVal
 
 
 def dump_json(dcm):
-    def _impl(ds):
-        result = {}
+    def _convert_to_dict(ds):
+        converted = {}
         for key in sorted(ds.keys()):
             element = ds[key]
-            if element.VR == "SQ":
-                tag_value = []
-                for dataset in element.value:
-                    tag_value.append(_impl(dataset))
-            else:
-                tag_value = {
-                    'vr': element.VR,
-                    'description': element.description(),
-                    'value': repval(element)
-                }
-            key = '{:04x},{:04x}'.format(element.tag.group, element.tag.elem)
-            result[key] = tag_value
+            dumped_key = str(key).strip('()')
+            dumped_value = [
+                _convert_to_dict(dataset) for dataset in element.value
+            ] if element.VR == "SQ" else {
+                'vr': element.VR,
+                'description': element.description(),
+                'value': dump_value(element)
+            }
+            converted[dumped_key] = dumped_value
+        return converted
 
-        return result
-
-    return json.dumps(_impl(dcm))
+    return json.dumps(_convert_to_dict(dcm))
 
 
 @click.option("--format", type=str, default=Format.PRETTY)
