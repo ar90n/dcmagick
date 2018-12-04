86a3419259e6060864009b9972593ba2f219d4b5
Masahiro Wada <argon.argon.argon@gmail.com>
2018-12-05 00:58:49 +0900

    Improve query

--- src/dcmagick/find/query.py
+++ src/dcmagick/find/query.py
@@ -1,9 +1,89 @@
 from pydicom import dcmread
 
 
+def _or(obj, value):
+    result = False
+    for query in value:
+        result |= _match(obj, query)
+    return result
+
+
+def _and(obj, value):
+    result = True
+    for query in value:
+        result &= _match(obj, query)
+    return result
+
+
+def _eq(obj, value):
+    return obj == value
+
+def _ne(obj, value):
+    return not _eq(obj, value)
+
+def _in(obj, value):
+    return obj in value
+
+def _gt(obj, value):
+    return value < obj
+
+def _gte(obj, value):
+    return _gt(obj, value) or _eq(obj, value)
+
+def _lt(obj, value):
+    return not _gte(obj, value)
+
+def _lte(obj, value):
+    return not _gt(obj, value)
+
+
+def _get_op(key):
+    _eq_with_key = lambda obj, value: _eq(obj[key], value)
+    return {
+      '$eq': _eq,
+      '$ne': _ne,
+      '$in': _in,
+      '$gt': _gt,
+      '$gte': _gte,
+      '$lt': _lt,
+      '$lte': _lte,
+    }.get(key, _eq_with_key)
+
+
+def _match(obj, query):
+    """
+    >>> _match({}, {})
+    True
+    >>> _match({'a': 100}, {'a': 100})
+    True
+    >>> _match({'a': 100}, {'a': 200})
+    False
+    >>> _match({'a': {'b': {'c': 'd'}}}, {'a': {'b' : {'c': 'd'}}})
+    True
+    >>> _match({'a': 'b'}, {'a': {'$in' : ['c', 'd']}})
+    False
+    >>> _match({'a': 'b'}, {'a': {'$in' : ['c', 'b']}})
+    True
+    >>> _match({'a': 100}, {'a': {'$gt' : 50, '$lt': 200}})
+    True
+    >>> _match({'a': 100}, {'a': {'$gt' : 50, '$lte': 100}})
+    True
+    >>> _match({'a': 100}, {'a': {'$ne' : 50}})
+    True
+    """
+    result = True
+    for key, value, in query.items():
+        if isinstance(value, dict):
+            ret = _match(obj[key], value)
+        else:
+            ret = _get_op(key)(obj, value)
+        result &= ret
+    return result
+
 def match(query, path):
     try:
         dcm = dcmread(str(path))
+        result = _match(dcm, query)
     except:
         return False
-    return True
+    return result
