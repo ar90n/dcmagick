fbb7c8065b47abd5d0f953deff3be1044f6d129b
Masahiro Wada <argon.argon.argon@gmail.com>
2018-12-14 03:39:29 +0900

    Add window calcuration from input

--- src/dcmagick/render/slice.py
+++ src/dcmagick/render/slice.py
@@ -33,6 +33,28 @@ def slice_loader(src, dst):
         yield output_path, dcm
 
 
+def calc_window(pixel_array, *, wc=None, ww=None):
+    minv = np.min(pixel_array)
+    maxv = np.max(pixel_array)
+
+    wc = (maxv + minv) // 2 if wc is None else wc
+    ww = 2 * min(abs(minv - wc), abs(maxv - wc)) if ww is None else ww
+    return wc, ww
+
+
+def apply_window(pixel_array, wc, ww):
+    half_width = ww // 2
+    minv = wc - half_width
+    maxv = wc + half_width
+
+    window_applied = pixel_array.copy()
+    window_applied[pixel_array < minv] = minv
+    window_applied[maxv < pixel_array] = maxv
+    window_applied = 255.0 * (window_applied - minv) / (maxv - minv)
+    window_applied = window_applied.astype(np.uint8)
+    return window_applied
+
+
 @click.command()
 @click.option('--wc', type=int, help='Window center')
 @click.option('--ww', type=int, help='Window width')
@@ -45,6 +67,6 @@ def render(wc, ww, cmap, gsps, kos, src, dst):
     format = _get_format(dst)
 
     for path, dcm in slice_loader(src, dst):
-        tmp = 255.0 * (dcm.pixel_array - np.min(dcm.pixel_array)) / (np.max(dcm.pixel_array) - np.min(dcm.pixel_array))
-        tmp = tmp.astype(np.uint8)
-        skimage.io.imsave(path, tmp)
+        wc, ww = calc_window(dcm.pixel_array, wc=wc, ww=ww)
+        buffer = apply_window(dcm.pixel_array, wc, ww)
+        skimage.io.imsave(path, buffer)
