269f557879bbf6b622075337636daee8d4885d93
Masahiro Wada <argon.argon.argon@gmail.com>
2019-04-05 12:53:41 +0000

    Add multi process support to find subcommand

--- src/dcmagick/find/find.py
+++ src/dcmagick/find/find.py
@@ -3,15 +3,18 @@ from pathlib import Path
 
 import click
 
-from ..common.query import match
+from joblib import Parallel, delayed
+
 from ..common.context import slice_context
+from ..common.query import match
 
 
 @click.command()
+@click.option("-p", type=int, default=1, help="Number of process.")
 @click.option("--name", type=str, help="File name pattern to find dicom file.")
 @click.option("--query", type=str, help="Query to find dicom file.")
 @click.argument("root", nargs=1)
-def find(name, query, root):
+def find(p, name, query, root):
     name = name or "*"
     query = query or "{}"
     query_obj = json.loads(query)
@@ -19,8 +22,12 @@ def find(name, query, root):
         click.echo("{} is not supported query.".format(query))
         return
 
-    for path in Path(root).glob("**/{}".format(name)):
-        with slice_context(path, None) as  proxy_and_param:
-            slice_proxy, proc_params = proxy_and_param
+    def _find_task(path):
+        with slice_context(str(path), None) as proxy_and_param:
+            slice_proxy, _ = proxy_and_param
             if match(query_obj, slice_proxy):
                 click.echo(path)
+
+    Parallel(n_jobs=p)(
+        [delayed(_find_task)(p) for p in Path(root).glob("**/{}".format(name))]
+    )
