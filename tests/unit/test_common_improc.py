aa3bd475efc197c7b7b1378411c0b55a62b790c1
Masahiro Wada <argon.argon.argon@gmail.com>
2019-04-15 14:46:19 +0000

    Add travis ci settings

--- tests/unit/test_common_improc.py
+++ tests/unit/test_common_improc.py
@@ -10,4 +10,4 @@ from dcmagick.common import improc
     (np.array([1, 2, 3], dtype=np.uint8), np.array([0, 0.5, 1.0], dtype=np.float32))
 ])
 def test_normalize_cast(input, expect):
-    assert np.allclose(improc.normalize_cast(input, expect.dtype), expect)
+    assert np.allclose(improc.normalize_cast(input, dtype=expect.dtype), expect)
